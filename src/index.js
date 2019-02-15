8da1e4fd0372092aace18f26e923b684c97f093e
xiaoxiangming <xiaoxm.ok@foxmail.com>
2019-02-15 12:02:52 +0800

    add

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import Loading from './components/loading.vue'
+
+let $vm
+
+export default {
+    install(Vue, options){
+        if(!$vm) {
+            const LoadingPlugin = Vue.extend(Loading)
+
+            $vm = new LoadingPlugin({
+                el: document.createElement('div')
+            })
+            document.body.appendChild($vm.el)
+        }
+
+        $vm.show = false
+
+        let loading = {
+            show(text) {
+                $vm.show = true
+                $vm.text = text
+            },
+            hide() {
+                $vm.show = false
+            }
+        }
+
+        if(!Vue.$loading) {
+            Vue.$loading = loading
+        }
+
+        // Vue.prototype.$loading = Vue.$loading
+
+        Vue.mixin({
+            created() {
+                this.$loading = Vue.$loading
+            }
+        })
+    }
+}
